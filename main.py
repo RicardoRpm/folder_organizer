71de231b8cf60db69227b24a7fc5636dadea9f7b
RicardoRpm <Ricardomiguel190@gmail.com>
2024-05-05 08:30:07 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+
+sourceFolder = "C:\Users\juset\Downloads"
+destinationFolder = "C:\Users\juset\Downloads"
+
+print("####### HELLO FOLDER ORGANIZER ######")
+print("Source Folder: ", sourceFolder)
+
