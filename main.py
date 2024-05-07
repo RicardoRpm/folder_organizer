7cf967cb410bd4d88ecb62832d7f41d92a9b36d0
RicardoRpm <Ricardomiguel190@gmail.com>
2024-05-07 22:15:37 +0100

    Read all file in path download

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from shutil import copytree, ignore_patterns, move, copy
+import os
 
-sourceFolder = "C:\Users\juset\Downloads"
-destinationFolder = "C:\Users\juset\Downloads"
+sourceFolder = "C:\\Users\\juset\\Downloads"
+destinationFolderMusic = "C:\\Users\\juset\\Music"
+destinationFolderImage = "C:\\Users\\juset\\Pictures"
+destinationFolderImage = "C:\\Users\\juset\\Pictures"
 
-print("####### HELLO FOLDER ORGANIZER ######")
-print("Source Folder: ", sourceFolder)
+#copy(sourceFolder, destinationFolder);
 
+listFiles = os.listdir(sourceFolder)
+
+for file in listFiles:
+    if  (file.endswith(".mp3")) or  (file.endswith(".wav")):
