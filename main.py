da52dd698fce14c024e35a7368998b65e2c764c0
RicardoRpm <Ricardomiguel190@gmail.com>
2024-05-07 22:41:38 +0100

    Counting file copied

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 from shutil import copytree, ignore_patterns, move, copy
 import os
 
-sourceFolder = "C:\\Users\\juset\\Downloads"
-destinationFolderMusic = "C:\\Users\\juset\\Music"
-destinationFolderImage = "C:\\Users\\juset\\Pictures"
-destinationFolderImage = "C:\\Users\\juset\\Pictures"
+SRC_FOLDER = "C:\\Users\\juset\\Downloads"
+DST_FOLDER_MUSIC = "C:\\Users\\juset\\Music\\Downloaded"
+DST_FOLDER_IMAGE = "C:\\Users\\juset\\Pictures\\Downloaded"
+DST_FOLDER_DOCUMENTS = "C:\\Users\\juset\\Documents\\Downloaded"
+DST_FOLDER_VIDEOS = "C:\\Users\\juset\\Documents\\Videos"
 
-#copy(sourceFolder, destinationFolder);
+countMusic = 0
+countImage = 0
+countDocuments = 0
+countVideos = 0
 
-listFiles = os.listdir(sourceFolder)
+listFiles = os.listdir(SRC_FOLDER)
 
 for file in listFiles:
-    if  (file.endswith(".mp3")) or  (file.endswith(".wav")):
+    if  (file.endswith(".mp3")) or (file.endswith(".wav")):
+        move(SRC_FOLDER, DST_FOLDER_MUSIC)
+        countMusic = countMusic + 1
+    elif (file.endswith(".mp4")): 
+        move(SRC_FOLDER, DST_FOLDER_VIDEOS)
+        countVideos = countVideos + 1
+    elif (file.endswith(".png")) or  (file.endswith(".jpg")) or (file.endswith(".jpeg")): 
+        move(SRC_FOLDER, DST_FOLDER_IMAGE)
+        countImage = countImage + 1
+    elif (file.endswith(".pdf")) or (file.endswith(".doc")) or (file.endswith(".txt")) or (file.endswith(".docx")) or (file.endswith(".rtf")) or (file.endswith(".pptx")): 
+        move(SRC_FOLDER, DST_FOLDER_DOCUMENTS)
+        countDocuments = countDocuments + 1
+    else:
+        continue
+
+print("RESUMO DOS ARQUIVOS MOVIDOS")
+print("VIDEOS: ", countVideos)
+print("IMAGES: ", countImage)
+print("DOCUMENTS: ", countDocuments)
+print("MUSICAS: ", countMusic)
